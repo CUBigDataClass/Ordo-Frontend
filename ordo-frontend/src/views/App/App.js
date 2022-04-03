3e287f79fe787cc4a0f95704f20b44f48efab25e
Joshua Truong <joshktruong@gmail.com>
2022-04-03 01:47:27 -0600

    app page

--- ordo-frontend/src/views/App/App.js
+++ ordo-frontend/src/views/App/App.js
@@ -1,12 +1,16 @@
 
-import Home from '../Home/Home';
+// import Home from '../Home/Home';
+import Movies from '../Movies/Movies';
 import Navigation from '../../components/Navigation/Navigation';
 
 function App() {
   return (
     <div>
       <Navigation />
-      <Home />
+      <div style={{margin:'0 10% 0 10%'}}>
+        {/* <Home /> */}
+        <Movies />
+      </div>
     </div>
   );
 }
