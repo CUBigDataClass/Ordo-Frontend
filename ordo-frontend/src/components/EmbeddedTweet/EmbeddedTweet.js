7ee22e149fded508a27cc6250515bc223cdbd450
Joshua Truong <joshktruong@gmail.com>
2022-04-06 12:16:17 -0600

    Header title

--- ordo-frontend/src/components/EmbeddedTweet/EmbeddedTweet.js
+++ ordo-frontend/src/components/EmbeddedTweet/EmbeddedTweet.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Tweet } from 'react-twitter-widgets'
 
+import { Typography } from '@mui/material';
 
 const EmbeddedTweet = () => {
   var tweets = ['1491255003268644866', '1383105268448305155', '463440424141459456']
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
-    <div style={{width:'60vh', height:'auto', margin:'1%'}}>
-      <div style={{height:'100vh', overflow:'auto'}}>
+    <div style={{width:'40vh', height:'auto', margin:'0 1% 1% 1%', float:'center'}}>
+      <Typography variant='h5'>Twitter Review's</Typography>
+      <div style={{height:'90vh', overflow:'auto'}}>
         {
           tweets.map((tweet) => (
             <div key={tweet}>
