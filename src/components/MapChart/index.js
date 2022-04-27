20d42e273face83c73faf13ada2afaefbd28de5d
Joshua Truong <joshktruong@gmail.com>
2022-04-26 23:05:41 -0600

    progress bar

--- src/components/MapChart/index.js
+++ src/components/MapChart/index.js
@@ -15,11 +15,11 @@ const geoUrl =
 // Remove this once you have new data
 const rounded = num => {
     if (num > 1000000000) {
-        return Math.round(num / 100000000) / 10 + "Bn";
+        return Math.round(num / 100000000) / 10 + "%";
     } else if (num > 1000000) {
-        return Math.round(num / 100000) / 10 + "M";
+        return Math.round(num / 100000) / 10 + "%";
     } else {
-        return Math.round(num / 100) / 10 + "K";
+        return Math.round(num / 100) / 10 + "%";
     }
 };
 
